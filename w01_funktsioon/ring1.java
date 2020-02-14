8ea8405868cf293a5986e620667eacefe0534a1e
Daniel Eelmaa <55189830+daneelm@users.noreply.github.com>
2020-02-14 13:49:12 +0200

    Add files via upload

--- w01_funktsioon/ring1.java
+++ w01_funktsioon/ring1.java
@@ -1,20 +1,26 @@
 import java.lang.Math.*;
 public class ring1{
-    public static void main(String[] arg){
-        if(arg.length!=1){
-            System.out.println("Palun sisesta ringi raadius");
-            return;
-        }
-        int arv=Integer.parseInt(arg[0]);
+    public static double arvutus(int arv){
+        
         double S = 0;
         double P = 0;
         S = Math.PI * arv * arv;
         P = Math.PI * 2 * arv;
         System.out.println("Ringi pindala on " + S);
         System.out.println("Ringi raadius on " + P);
+        return S;
+    }
+
+    public static void main(String[] arg){
+        if(arg.length!=1){
+            System.out.println("Palun sisesta ringi raadius");
+            return;
+        }
+        int arv=Integer.parseInt(arg[0]);
+        arvutus(arv);
     }
 }
-
+/*
 [danieeel@greeny 01]$ java ring1 8
 Ringi pindala on 201.06192982974676
-Ringi raadius on 50.26548245743669
+Ringi raadius on 50.26548245743669*/
